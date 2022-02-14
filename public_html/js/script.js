e4c584d8b205fc1e5f27a4838c56c65aa7d20ab5
Mike <MZbrozek@cnm.edu>
2022-02-14 10:46:41 -0700

    Starting to build the backend src for the contact form

---
+++ public_html/js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function (){
+    $('#contact').validate({
+        debug: true,
+        errorClass: 'alert alert-danger',
+        ErrorLabelContainer: '#output-area',
+        errorElement: 'div',
+    //
+    //rules are for name attribute values
+        rules: {
+            name: {
+                required: true
+            },
+            email: {
+                email: true,
+                required: true
+            },
+            // skipping subject
+            message: {
+                required: true,
+                maxlength: 2000
+            }
+        },
+        messages: {
+            name: {
+                required: 'Please add your name.'
+            },
+            email: {
+                email: 'Please provide a valid email.',
+                required: 'Must provide a return email'
+            },
+            message: {
+                required: 'Cannot send email without a message.',
+                maxlength: 'Message is too long, 2000 character max.'
+            }
+        },
+        submitHandler: (form) => {
+            $('#contact').ajaxSubmit({
+                type: 'POST',
+                url: $('#contact').attr('action'),
+                success: (ajaxOutput) => {
+                    $('#output-area').css('display', '')
+                        $('#output-area').html(ajaxOutput)
+
+                    if($('.alert-success') >= 1) {
+                        $('#contact')[0].reset()
+                    }
+
+                }
+            })
+        }
+    })
+})
