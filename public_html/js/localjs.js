248a83da73844e704e2e98664b5ac4a6f1692284
Mike <MZbrozek@cnm.edu>
2022-02-20 22:40:15 -0700

    Reworked div structure in portfolio section to display text. Wrote some of the content, but need to expand.

--- public_html/js/localjs.js
+++ public_html/js/localjs.js
@@ -3,12 +3,12 @@ console.log('Is this thing on??')
 // Portfolio card rotate -----------//
 const portfolioCard = document.getElementById('rotate-wrap')
 
-function rotateCard(e) {
-    this.style.setProperty('--rotate', 1.8)
+// function rotateCard(e) {
+//     this.style.setProperty('--rotate', 1.8)
 }
 
-function rotateBack() {
-    this.style.setProperty('--rotate', -100)
+// function rotateBack() {
+//     this.style.setProperty('--rotate', -100)
     // console.log(this.event) This works and logs the correct event, but does not apply the rotation.
 }
 
