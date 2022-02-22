45b19cfd82e28e057b98abf2c767ab9fda2a4326
Mike <MZbrozek@cnm.edu>
2022-02-21 20:53:53 -0700

    Final adjustments to hero banner text

--- public_html/js/localjs.js
+++ public_html/js/localjs.js
@@ -1,17 +1,3 @@
 console.log('Is this thing on??')
 
-// Portfolio card rotate -----------//
-const portfolioCard = document.getElementById('rotate-wrap')
-
-// function rotateCard(e) {
-//     this.style.setProperty('--rotate', 1.8)
-}
-
-// function rotateBack() {
-//     this.style.setProperty('--rotate', -100)
-    // console.log(this.event) This works and logs the correct event, but does not apply the rotation.
-}
-
-portfolioCard.addEventListener('mouseenter', rotateCard)
-portfolioCard.addEventListener('mouseleave', rotateBack)
 
