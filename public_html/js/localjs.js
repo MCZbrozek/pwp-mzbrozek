b07346d507a6c66e96219007d7ea29a7bd951764
Mike <MZbrozek@cnm.edu>
2022-02-23 07:48:22 -0700

    Last minute style changes and secret code javascript

--- public_html/js/localjs.js
+++ public_html/js/localjs.js
@@ -1,3 +1,23 @@
 console.log('Is this thing on??')
 
 
+//added secret code to page, go ahead, try it! //
+const keysPressed = []
+const secretCode = 'mz'
+
+ window.addEventListener("keyup", (e) => {
+    console.log(e.key)
+    keysPressed.push(e.key)
+    keysPressed.splice(-secretCode.length - 1, keysPressed.length - secretCode.length) //truncate our array to match the length of secret code.
+    if(keysPressed.join('').includes(secretCode)) {
+        console.log('Magic')
+        cornify_add()
+    }
+    console.log(keysPressed)
+})
+
+
+
+
+
+
